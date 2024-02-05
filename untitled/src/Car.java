02b07ca01c1bc402bcaf1d4660ba85f01a6d08cc
prince.kumar <prince.kumar@pragmaticplay.com>
2024-02-05 11:54:08 +0530

    setter added

--- untitled/src/Car.java
+++ untitled/src/Car.java
@@ -3,4 +3,11 @@ public class Car {
     private int noOfWheel ;
     private String brand ;
 
+    public void setNoOfWheel(int noOfWheel) {
+        this.noOfWheel = noOfWheel;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
