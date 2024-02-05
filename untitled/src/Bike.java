ab2307288d8725da1fc0e00eeae4e206110ca87f
prince.kumar <prince.kumar@pragmaticplay.com>
2024-02-05 14:36:25 +0530

    LCE-27599 commit in main class

--- untitled/src/Bike.java
+++ untitled/src/Bike.java
@@ -1,4 +1,20 @@
 public class Bike {
     int wheel ;
     double cost ;
+
+    public int getWheel() {
+        return wheel;
+    }
+
+    public void setWheel(int wheel) {
+        this.wheel = wheel;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 }
