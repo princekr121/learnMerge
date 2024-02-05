70489b59f0da870dd96a9a5f652dd0890efbc305
prince.kumar <prince.kumar@pragmaticplay.com>
2024-02-05 11:52:38 +0530

    getter added

--- untitled/src/Car.java
+++ untitled/src/Car.java
@@ -3,4 +3,11 @@ public class Car {
     private int noOfWheel ;
     private String brand ;
 
+    public int getNoOfWheel() {
+        return noOfWheel;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
 }
